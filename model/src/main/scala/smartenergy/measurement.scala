b86051e2aabd447d97e5f7f6dc130e347ac8c0ae
Tom Lous <tomlous@gmail.com>
2022-03-31 15:06:15 +0200

    Cache

--- model/src/main/scala/smartenergy/measurement.scala
+++ model/src/main/scala/smartenergy/measurement.scala
@@ -1,20 +1,4 @@
 /** MACHINE-GENERATED FROM AVRO SCHEMA. DO NOT EDIT DIRECTLY */
 package smartenergy
 
-final case class measurement(
-  msg_id: String,
-  metering_point_id: String,
-  channel_id: String,
-  timestamp_ms: Long,
-  timestamp: java.time.Instant,
-  value: BigDecimal,
-  register_reading: Option[BigDecimal] = None,
-  period_covered_ms: Option[Long] = None,
-  is_quality_disturbed: Option[Boolean] = None,
-  is_metering_disturbed: Option[Boolean] = None,
-  is_accurate: Option[Boolean] = None,
-  is_corrected: Option[Boolean] = None,
-  is_estimated: Option[Boolean] = None,
-  source: String,
-  last_updated: java.time.Instant
-)
+final case class measurement(msg_id: String, metering_point_id: String, channel_id: String, timestamp_ms: Long, timestamp: java.time.Instant, value: BigDecimal, register_reading: Option[BigDecimal] = None, period_covered_ms: Option[Long] = None, is_quality_disturbed: Option[Boolean] = None, is_metering_disturbed: Option[Boolean] = None, is_accurate: Option[Boolean] = None, is_corrected: Option[Boolean] = None, is_estimated: Option[Boolean] = None, source: String, last_updated: java.time.Instant)
